bf942eb7453842cf84c5ea7550d57e98cc4ff507
Jason Adams <jwa002@ucsd.edu>
2018-10-10 10:57:02 -0700

    tutorials

---
+++ new_input.py
@@ -0,0 +1,8 @@
+
+def new_input(a,b):
+    if b>a:
+        print(a,b)
+    elif a>b: 
+        print(b,a)
+    else:
+        print("unclear")
---
+++ newer_input.py
@@ -0,0 +1,8 @@
+
+def newer_input(a,b):
+    if b>a:
+        print (a,b)
+    elif a>b:
+        print(b,a)
+    else:
+        print("Please try correct listing")
